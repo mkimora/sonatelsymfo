3e2d2fb3eb1e423b987851ab66205169f5e512be
mkimora <49501287+mkimora@users.noreply.github.com>
2019-07-15 00:03:26 +0200

    Add files via upload

---
+++ sonatelsymfony/src/Controller/EmployeController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Employe;
+use App\Entity\Service;
+use App\Entity\Repository;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class EmployeController extends AbstractController
+{
+    /**
+     * @Route("/employe", name="employe")
+     */
+    public function index()
+    {
+        return $this->render('employe/index.html.twig', [
+            'controller_name' => 'EmployeController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('employe/home.html.twig');
+    }
+
+    /**
+     * @Route("employe/new", name="sonatel_create")
+     */
+    public function form(Employe $employe=null, Request $request, ObjectManager $manager)
+    {
+        if(!$employe){
+            $employe = new Employe();
+        }
+        $form = $this->createFormBuilder($employe)
+            ->add('matricule')
+            ->add('nomcomplet')
+            ->add('datenaiss', DateType::class, [
+                'widget' => "single_text",
+                'format' => "yyyy-MM-dd"
+            ])
+            ->add('salaire')
+            // ->add('id',EntityType::class,[ 
+            //     'class' => Service::class,
+            //     'choice_label' => 'libelle',
+            // ])
+            ->getForm();
+
+        $form->handlerequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($employe);
+            $manager->flush();
+
+            return $this->redirectToRoute('sonatel_liste', [
+                'id' => $employe->getId()
+            ]);
+        }
+
+        return $this->render('employe/create.html.twig', [
+            'formEmploye' => $form->createView()
+        ]);
+    }
+
+
+    /**
+     * @Route("employe/liste", name="sonatel_liste")
+     */
+
+    public function liste()
+    {
+        $repo = $this->getDoctrine()->getRepository(Employe::class);
+
+        $employes = $repo->findAll();    
+
+        return $this->render('employe/liste.html.twig', [
+            'employes' => $employes
+        ] );
+    }
+
+
+
+}
---
+++ sonatelsymfony/src/Entity/Employe.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EmployeRepository")
+ */
+class Employe
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     
+     */
+    private $matricule;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nomcomplet;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $datenaiss;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $salaire;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMatricule(): ?string
+    {
+        return $this->matricule;
+    }
+
+    public function setMatricule(string $matricule): self
+    {
+        $this->matricule = $matricule;
+
+        return $this;
+    }
+
+    public function getNomcomplet(): ?string
+    {
+        return $this->nomcomplet;
+    }
+
+    public function setNomcomplet(string $nomcomplet): self
+    {
+        $this->nomcomplet = $nomcomplet;
+
+        return $this;
+    }
+
+    public function getDatenaiss(): ?\DateTimeInterface
+    {
+        return $this->datenaiss;
+    }
+
+    public function setDatenaiss(\DateTimeInterface $datenaiss): self
+    {
+        $this->datenaiss = $datenaiss;
+
+        return $this;
+    }
+
+    public function getSalaire(): ?int
+    {
+        return $this->salaire;
+    }
+
+    public function setSalaire(int $salaire): self
+    {
+        $this->salaire = $salaire;
+
+        return $this;
+    }
+}
